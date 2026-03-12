bcbe40195959579b01bab0771bd4398a634f9513
sanjanagudepu77-cmd <sanjanagudepu77@gmail.com>
2026-03-12 20:57:28 +0530

    Time: 69 ms (23.53%) | Memory: 19.4 MB (54.21%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        '''n=len(nums)//2
+        ans=[]
+        for i in range(n):
+            ans.append(nums[i])
+            ans.append(nums[i+n])
+        return ans'''
+        ans=[]
+        for i in range(n):
+            ans.append(nums[i])
+            ans.append(nums[i+n])
+        return ans
+
+        
