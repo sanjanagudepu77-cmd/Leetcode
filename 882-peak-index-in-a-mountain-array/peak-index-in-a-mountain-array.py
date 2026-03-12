e1b0f102d7f7c4da3a3a3fa54be7b04f66e553d8
sanjanagudepu77-cmd <sanjanagudepu77@gmail.com>
2026-03-12 19:14:53 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (88.74%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        '''for i in range(1,len(arr)):
+            if arr[i]>arr[i-1] and arr[i]>arr[i+1]:
+               return i''' 
+        l=0
+        r=len(arr)-1
+        while l<r:
+            mid=(l+r)//2
+            if arr[mid]<arr[mid+1]:
+                l=mid+1
+            else:
+                r=mid
+        return r
+
+        
+       
+        
+        
+        
