3a78582caf67d9fe81b61c63431038295880851d
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-03-17 14:25:50 +0530

    Time: 440 ms (78.99%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT product_id, new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN (
+  SELECT product_id, MAX(change_date)
+  FROM Products
+  WHERE change_date <= '2019-08-16'
+  GROUP BY product_id
+)
+
+UNION
+
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING COUNT(CASE WHEN change_date <= '2019-08-16' THEN 1 END) = 0;
