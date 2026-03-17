e8b6b9d513b94cccf9c91c3a585407e020fdab79
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-03-17 09:37:11 +0530

    Time: 1490 ms (5.40%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 SELECT a.student_id,a.student_name,b.subject_name,COUNT(c.subject_name) AS attended_exams
 FROM Students a
-CROSS JOIN Subjects b
+CROSS JOIN Subjects b   #every student is paired with every subject
 LEFT JOIN Examinations c
 ON a.student_id=c.student_id
 AND b.subject_name=c.subject_name
