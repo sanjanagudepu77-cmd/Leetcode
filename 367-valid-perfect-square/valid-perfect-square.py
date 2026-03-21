74d254267735678df16ccbf2fd7934d5428c9261
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-03-21 10:46:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (80.31%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -1,7 +1,18 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        sqrt = num ** 0.5
+        '''sqrt = num ** 0.5
         if sqrt % 2 == 0 or sqrt % 2 == 1:
             return True
+        return False'''
+        left = 0
+        right = num
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == num:
+               return True
+            elif mid * mid < num:
+               left = mid + 1
+            else:
+               right = mid - 1
         return False
         
