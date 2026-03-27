3861ee88679d5f153024dfacbf8b39d2cb76573f
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-03-27 20:16:51 +0530

    Time: 0 ms (100.00%) | Memory: 26.9 MB (96.11%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -5,12 +5,13 @@ class Solution:
             return nums[0]
         if nums[n-1]!=nums[n-2]:
             return nums[n-1]
-        l,h=0,n-2
+        l=0
+        h=n-1
         while l<=h:
             mid=(l+h)//2
             if nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1]:
                 return nums[mid]
-            if mid%2==1 and nums[mid]==nums[mid-1] or mid%2==0 and nums[mid]==nums[mid+1]:
+            if (mid%2==1 and nums[mid]==nums[mid-1]) or (mid%2==0 and nums[mid]==nums[mid+1]):
                 l=mid+1
             else:
                 h=mid-1
