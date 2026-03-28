dd86e07c9c6ff6476bb80abd40f27baa1abcac60
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-03-28 09:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (22.63%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        n=len(nums)
+        '''n=len(nums)
         if n==1:
             return nums[0]
         if nums[n-1]!=nums[n-2]:
@@ -15,5 +15,33 @@ class Solution:
                 l=mid+1
             else:
                 h=mid-1
+        return -1'''
+        n = len(nums)
+
+        # edge cases
+        if n == 1:
+            return nums[0]
+        if nums[0] != nums[1]:
+            return nums[0]
+        if nums[n - 1] != nums[n - 2]:
+            return nums[n - 1]
+
+        l = 1
+        h = n - 2
+
+        while l <= h:
+            mid = (l + h) // 2
+
+            # single element found
+            if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]:
+                return nums[mid]
+
+            # correct pair pattern on left side
+            if (mid % 2 == 1 and nums[mid] == nums[mid - 1]) or \
+               (mid % 2 == 0 and nums[mid] == nums[mid + 1]):
+                l = mid + 1
+            else:
+                h = mid - 1
+
         return -1
         
