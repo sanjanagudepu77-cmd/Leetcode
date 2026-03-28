3cc586ae58261276b0a594fe0a595cec58477bb5
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-03-28 12:40:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (63.56%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -6,36 +6,26 @@ class Solution:
             if curr > nums[i + 1]:
                 return i
         return n - 1'''
-        
         n = len(nums)
-
         if n == 1:
             return 0
-
         if nums[0] > nums[1]:
             return 0
-
         if nums[n - 1] > nums[n - 2]:
             return n - 1
-
         l = 1
         h = n - 2
-
         while l <= h:
             mid = (l + h) // 2
-
             # peak found
             if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
                 return mid
-
             # increasing slope → move right
             elif nums[mid] > nums[mid - 1]:
                 l = mid + 1
-
             # decreasing slope → move left
             else:
                 h = mid - 1
-
         return -1
 
         
