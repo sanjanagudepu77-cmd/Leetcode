04d00f120badda18f2625516b658496edfc6bce8
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-05-05 20:50:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (5.68%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,6 +1,6 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n=len(nums)
+        '''n=len(nums)
         res=[]
         for val in range(1<<n):
             s=[]
@@ -8,5 +8,18 @@ class Solution:
                 if val&(1<<j):
                     s.append(nums[j])
             res.append(s)
-        return res
+        return res'''
+        def generate(ind,subset,ans,nums):
+            if(ind==len(nums)):
+                ans.append(subset.copy())
+                return
+            subset.append(nums[ind])
+            generate(ind+1,subset,ans,nums)
+            subset.pop()
+            generate(ind+1,subset,ans,nums)
+        ind=0
+        ans=[]
+        subset=[]
+        generate(ind,subset,ans,nums)
+        return ans
         
