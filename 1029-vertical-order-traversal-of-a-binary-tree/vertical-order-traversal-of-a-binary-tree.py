9fca29258d300a77e88a92ca92b362395ee6201b
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-05-23 17:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (59.67%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        q=[(root,0,0)]
+        '''q=[(root,0,0)]
         colt=defaultdict(list)
         res=[]
         while q:
@@ -19,6 +19,24 @@ class Solution:
         for col in sorted(colt.keys()):
             node=sorted(colt[col])# node=sorted(colt[col],key=lambda x:[0],x[1])
             res.append([val for i,val in node]) #for row,val in node
+        return res'''
+        if not root:
+            return [] 
+        dict={}
+        q=[(root,0,0)]
+        while q:
+            node,row,col=q.pop(0)
+            if col not in dict:
+                dict[col]=[]
+            dict[col].append((row,node.val))
+            if node.left:
+                q.append((node.left,row+1,col-1))
+            if node.right:
+                q.append((node.right,row+1,col+1))
+        res=[]
+        for i in sorted(dict):
+            temp=sorted(dict[i])
+            res.append([v for r,v in temp])
         return res
 
 
