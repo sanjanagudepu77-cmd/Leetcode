2d87c970807810c1c5c0cc1716d144270d17bb97
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-05-27 11:46:26 +0530

    Time: 2 ms (80.74%) | Memory: 21.1 MB (64.66%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        inorder_map={}
+        '''inorder_map={}
         for i in range(len(inorder)):
             inorder_map[inorder[i]]=i
         self.pre_index=0
@@ -20,5 +20,55 @@ class Solution:
             root.left=helper(left,mid-1)
             root.right=helper(mid+1,right)
             return root
-        return helper(0,len(inorder)-1)
+        return helper(0,len(inorder)-1)'''
+        # Store inorder values with index
+        inMap = {}
+        for i in range(len(inorder)):
+            inMap[inorder[i]] = i
+
+        return self.build(
+            preorder, 0, len(preorder)-1,
+            inorder, 0, len(inorder)-1,
+            inMap
+        )
+
+    def build(self, preorder, preStart, preEnd,
+              inorder, inStart, inEnd, inMap):
+
+        # Base case
+        if preStart > preEnd or inStart > inEnd:
+            return None
+
+        # Root is first element in preorder
+        root = TreeNode(preorder[preStart])
+
+        # Find root in inorder
+        inRoot = inMap[root.val]
+
+        # Number of nodes in left subtree
+        numsLeft = inRoot - inStart
+
+        # Build left subtree
+        root.left = self.build(
+            preorder,
+            preStart + 1,
+            preStart + numsLeft,
+            inorder,
+            inStart,
+            inRoot - 1,
+            inMap
+        )
+
+        # Build right subtree
+        root.right = self.build(
+            preorder,
+            preStart + numsLeft + 1,
+            preEnd,
+            inorder,
+            inRoot + 1,
+            inEnd,
+            inMap
+        )
+
+        return root
         
