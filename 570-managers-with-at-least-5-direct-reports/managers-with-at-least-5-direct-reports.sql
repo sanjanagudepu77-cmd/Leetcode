36ac31d4228cd8555cf5b0a37d7b2c7134620880
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-09-21 11:57:33 +0530

    Time: 405 ms (37.32%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -2,7 +2,7 @@
 SELECT name 
 FROM Employee 
 WHERE id IN (
-    SELECT managerId 
-    FROM Employee 
-    GROUP BY managerId 
-    HAVING COUNT(*) >= 5)
+SELECT managerId 
+FROM Employee 
+GROUP BY managerId 
+HAVING COUNT(*) >= 5)
