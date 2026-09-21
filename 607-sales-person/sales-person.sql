ad4fa1bf7249eea3c44fdd702c9e391c4367f1c8
Sanjana Gudepu <sanjanagudepu77@gmail.com>
2026-09-21 14:29:38 +0530

    Time: 1320 ms (96.05%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -3,7 +3,7 @@ select s.name
 from SalesPerson s
 where s.name not in
 (select s.name
-from SalesPerson s
-left join Orders o on s.sales_id = o.sales_id
-left join Company c on o.com_id = c.com_id
-where c.name = 'Red')
+from salesperson as s
+left join orders o on s.sales_id=o.sales_id
+left join company c on c.com_id=o.com_id
+where c.name='red')
